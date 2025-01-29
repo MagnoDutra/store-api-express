bf9018ee74a7e5cf36f2dc57eed86cef4284048f
Magno <jmagno.dutra@hotmail.com>
2025-01-29 14:42:47 -0300

    Basic express app

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const connectDB = require("./db/connect");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store Api</h1><a href="/api/v1/products">products</a>');
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+async function start() {
+  try {
+    // await connectDB(process.env.MONGO);
+    app.listen(port, () => console.log(`Server running on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
