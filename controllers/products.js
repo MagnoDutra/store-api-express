96732899be74b1a4f1067741ba8efbdd05d10055
Magno <jmagno.dutra@hotmail.com>
2025-01-30 07:23:46 -0300

    Sort getAllProducts implemented

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,24 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const products = await Product.find().sort("name price");
   res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) queryObject.featured = featured === "true" ? true : false;
   if (company) queryObject.company = company;
   if (name) queryObject.name = { $regex: name, $options: "i" };
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    result = result.sort(sort.split(",").join(" "));
+  }
+
+  const products = await result;
   res.status(200).json({ products, length: products.length });
 };
 
