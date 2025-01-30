315779f5f654f7ffd784f7657aca5b7e70a35b1e
Magno <jmagno.dutra@hotmail.com>
2025-01-30 06:38:25 -0300

    Query params

--- controllers/products.js
+++ controllers/products.js
@@ -6,8 +6,8 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
-  res.status(200).json({ products });
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, length: products.length });
 };
 
 module.exports = {
