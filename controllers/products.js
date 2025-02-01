5d1a089e35c43c3f73fa3361af8a55b5c0e06502
Magno <jmagno.dutra@hotmail.com>
2025-01-31 23:00:57 -0300

    Numeric filters and project done

--- controllers/products.js
+++ controllers/products.js
@@ -28,6 +28,16 @@ const getAllProducts = async (req, res) => {
       regEx,
       (match) => `-${operatorMap[match]}-`
     );
+
+    const options = ["price", "rating"];
+
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: +value };
+      }
+    });
   }
 
   let result = Product.find(queryObject);
