3da6b252b885e312aa33b889ce99496a7a7a44f5
Magno <jmagno.dutra@hotmail.com>
2025-01-31 22:29:26 -0300

    Skip and Limit

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find().select("name price");
+  const products = await Product.find().select("name price").limit();
   res.status(200).json({ products });
 };
 
